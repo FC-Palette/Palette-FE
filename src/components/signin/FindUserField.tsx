22b1c84ab82334c8d7752689606a5ce399470c4f
dragon <sideoff0217@naver.com>
2023-09-08 21:38:53 +0900

    feat:implementing start & signIn pages component structure

---
+++ src/components/signin/FindUserField.tsx
@@ -0,0 +1,56 @@
+import { theme } from "styles/index";
+import { SIGNIN_FORM_TEXT } from "constants/index";
+import { styled } from "styled-components";
+
+
+export const FindUserField = () => {
+  return (
+    <FindUserFieldForm>
+        <FindIdWrap>{SIGNIN_FORM_TEXT.findIdText}</FindIdWrap>
+        <Divider />
+        <FindPasswordWrap>{SIGNIN_FORM_TEXT.findPasswordText}</FindPasswordWrap>
+    </FindUserFieldForm>
+  )
+}
+
+
+const FindUserFieldForm = styled.div`
+    display: flex;
+    width: 231px;
+    height: 35px;
+    margin: 0 auto;
+`
+
+
+const FindIdWrap = styled.div`
+    display: inline-block;  
+    padding: 9px 18px;
+    height: 35px;
+    font-size: 14px;
+    line-height: 16.71px;
+    color: ${theme.greyScale.grey5};  
+    text-decoration: underline;
+    text-underline-position: under;
+`
+
+const FindPasswordWrap = styled.div`
+    display: inline-block;  
+    padding: 9px 12px;
+    height: 35px;
+    font-size: 14px;
+    line-height: 16.71px;
+    color: ${theme.greyScale.grey5};   
+    text-decoration: underline;
+    text-underline-position: under;
+`
+
+
+const Divider = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1px;   
+    background-color: ${theme.greyScale.grey2};  
+    height: 20px; 
+    margin: auto 4px;
+`
--- src/constants/start/startTexts.ts
+++ src/constants/start/startTexts.ts
@@ -3,5 +3,7 @@ export const START_TEXTS = {
     mainTitleText: '오피스너',
     introFirstText: '지금까지 경험하지 못한',
     introSecondText: '스마트 오피스 생활을 경험해보세요!',
-    existenceText: '이미 계정이 있으시면?'
+    startOfficenerText: '오피스너 시작하기',
+    existenceText: '이미 계정이 있으시면?',
+    linkToSignInText: '로그인',
 }
