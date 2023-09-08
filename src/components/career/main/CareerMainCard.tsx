cbbc679ed1f89b34631ee957737b4a10b9b8b542
ahntaeuk <dotory0829@naver.com>
2023-09-08 21:40:47 +0900

    fix: git pull --rebase 충돌 해결

--- src/components/career/main/CareerMainCard.tsx
+++ src/components/career/main/CareerMainCard.tsx
@@ -14,6 +14,16 @@ export const CareerMainCard = () => {
       <CareerMainItemsWrap />
       <CareerMainItemsWrap />
       <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
+      <CareerMainItemsWrap />
 
       <CareerMainItemCreateButton />
     </Card>
@@ -23,8 +33,6 @@ export const CareerMainCard = () => {
 const Card = styled.div`
   position: relative;
   width: 100%;
-<<<<<<< HEAD
-=======
   top: 200px;
 
   /* height: auto; */
@@ -39,5 +47,4 @@ const Card = styled.div`
 
   /* width: auto; */
   /* height: 100vh; */
->>>>>>> fbf0980 (feat: 성장해요 메인 페이지 컴포넌트 생성)
 `
