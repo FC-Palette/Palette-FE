9e48ef25abafd0f168956d3c65c5cea6ead82777
ahntaeuk <dotory0829@naver.com>
2023-09-11 13:54:39 +0900

    feat: 모임생성-1, 다음 버튼 생성

--- src/pages/CareerCreateMeetingStepOne.tsx
+++ src/pages/CareerCreateMeetingStepOne.tsx
@@ -1,18 +1,29 @@
-import { StepProgressBar } from '@/components'
+import { Button, StepProgressBar } from 'components/common'
 import { CareerCreateMeetingHeader, StepOne } from '@/components/career'
 
 import styled from 'styled-components'
 export const CareerCreateMeetingStepOne = () => {
   return (
-    <Wrap>
+    <>
       <CareerCreateMeetingHeader />
       <StepProgressBar $currentPage={1} />
       <StepOne />
-    </Wrap>
+      <BtnWrap>
+        <Button
+          $btnWidth="382px"
+          $btnHeight="60px"
+          $fontSize="20px"
+          $borderRadius="8px">
+          다음
+        </Button>
+      </BtnWrap>
+    </>
   )
 }
-const Wrap = styled.div`
-  width: 430px;
-  height: 100%;
-  /* height: 932px; */
+
+const BtnWrap = styled.div`
+  font-size: 20px;
+  line-height: 26px;
+  font-weight: 500;
+  margin: 40px 24px 30px;
 `
