c4a9320aedee49b90ef50c5c5e0c945bd60056d7
hookor <gerrard1219@gmail.com>
2023-09-12 01:51:40 +0900

    feat: chatTexts.ts chat-texts listup

---
+++ src/constants/chat/chatTexts.ts
@@ -0,0 +1,20 @@
+const CHAT_TEXTS = {
+  header: '채팅',
+  groupChat: '단체톡',
+  personalChat: '개인톡',
+  groupInfo: '참여인원',
+  announcement: '공지',
+  exitHeader: '채팅방 나가기',
+  exitWarn:
+    '참여중인 모임원들에게 알린 후 모임을 삭제하셔야 이후 모임활동에 불이익이 없어요!',
+  cancel: '취소',
+  exit: '나가기',
+  delete: '삭제',
+  maintain: '유지하기',
+  copy: '복사',
+  register: '공지',
+  annList: '공지목록',
+  inDetail: '상세보기',
+  sendImg: '이미지 전송하기',
+  sendFile: '파일 전송하기'
+}
