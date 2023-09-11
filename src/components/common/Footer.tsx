f70e18e58a2d2e4c39d2ed9025f1c3bcf410273e
LeeYongSoo <sideoff0217@naver.com>
2023-09-12 03:22:10 +0900

    feat: apply header && footer in mypage

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -20,12 +20,12 @@ export const Footer = () => {
 const Wrapper = styled.footer`
   display: flex;
   align-items: center;
-  width: 100%;
+  width: 430px;
   height: 80px;
   justify-content: space-around;
   border-top: 2px solid ${theme.greyScale.grey2};
   z-index: 100;
-  position: sticky;
+  position: fixed;
   bottom: 0;
   background-color: ${theme.main.white};
 `
