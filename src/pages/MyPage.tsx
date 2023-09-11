d44130d3581cec100d06241cd91bcc2363f7e164
LeeYongSoo <sideoff0217@naver.com>
2023-09-12 03:22:10 +0900

    feat: apply header && footer in mypage

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,12 +1,38 @@
+import { Footer, Header } from "@/components"
+import { Notification, Setting2 } from "iconsax-react"
 import styled from "styled-components"
 
 
 export const MyPage = () => {
   return (
-    <Wrap>MyPage</Wrap>
+    <MyPageWrap>      
+      <Header
+    centerText="마이페이지">
+      <StyledIcon>
+      <Notification />
+      </StyledIcon>
+      <IconWrapper>
+      <Setting2 />
+      </IconWrapper>
+      </Header>
+      <Footer />
+    </MyPageWrap>
   )
 }
 
 
-const Wrap = styled.div`
+const MyPageWrap = styled.div`
+  position: relative;
+  height: 100vh;
+  
 `
+
+const IconWrapper = styled.div`
+  margin-left: 5px;
+`
+
+const StyledIcon = styled.div`
+  margin-right: 5px;
+`
+
+
