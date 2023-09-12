2e3794cb020e22bf4edbf88e498dcceba826d57f
hookor <gerrard1219@gmail.com>
2023-09-12 17:22:39 +0900

    feat: create ChatList && set Layout

--- src/pages/ChatList.tsx
+++ src/pages/ChatList.tsx
@@ -1,5 +1,5 @@
-import { Header } from 'components/index'
-
+import { Header, Footer, SubHeader, ChatRoom } from 'components/index'
+import { CHAT_TABS } from 'constants/index'
 /*
     HEADER
     TABS
@@ -11,6 +11,13 @@ export const ChatList = () => {
   return (
     <>
       <Header centerText="채팅" />
+      <SubHeader
+        items={CHAT_TABS}
+        initialItem={CHAT_TABS[0]}
+      />
+      {/* 작업하자 */}
+      <ChatRoom></ChatRoom>
+      <Footer />
     </>
   )
 }
