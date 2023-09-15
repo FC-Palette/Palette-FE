bb2b8356b7df1d67252d195a5685b179c5f74942
hookor <gerrard1219@gmail.com>
2023-09-15 17:20:38 +0900

    feat: word-break:break-all applied in ChatPreview

--- src/components/chat/chatlist/PreviewContent.tsx
+++ src/components/chat/chatlist/PreviewContent.tsx
@@ -1,8 +1,12 @@
 import { styled } from 'styled-components'
 export const PreviewContent = () => {
-  return <Wrapper>가나다라마바사아자차카타파하가나다라마</Wrapper>
+  return (
+    <Wrapper>asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasas</Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
   max-height: 34px;
+  overflow: hidden;
+  word-break: break-all;
 `
