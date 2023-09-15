11d0132518f6af2a302ba717f1f64a767e1230dd
hookor <gerrard1219@gmail.com>
2023-09-15 17:20:38 +0900

    feat: word-break:break-all applied in ChatPreview

--- src/components/chat/chatlist/PreviewTitle.tsx
+++ src/components/chat/chatlist/PreviewTitle.tsx
@@ -1,3 +1,10 @@
+import { styled } from 'styled-components'
 export const PreviewTitle = () => {
-  return <div>PreviewTitle</div>
+  return <Wrapper>asdasdasdasdasdasdasdasdasdasasdasdasdas</Wrapper>
 }
+const Wrapper = styled.div`
+  max-height: 17px;
+  overflow: hidden;
+  font-weight: 600;
+  word-break: break-all;
+`
