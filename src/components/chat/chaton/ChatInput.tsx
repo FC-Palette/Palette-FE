bca33a41da748ebbf70a3df3dfc03666f7bb907c
hookor <gerrard1219@gmail.com>
2023-09-17 18:52:56 +0900

    feat: initially checked ChatInputField working and created ChatInfo.tsx

---
+++ src/components/chat/chaton/ChatInput.tsx
@@ -0,0 +1,39 @@
+import { styled } from 'styled-components'
+import { useRef, useEffect } from 'react'
+
+export const ChatInput = () => {
+  const inputRef = useRef<HTMLTextAreaElement>(null)
+  useEffect(() => {}, [])
+
+  useEffect(() => {
+    handleResizeHeight()
+  }, [])
+
+  const handleResizeHeight = () => {
+    if (inputRef?.current) {
+      inputRef.current.style.height = 'auto' //height 초기화
+      inputRef.current.style.height = inputRef?.current.scrollHeight + 'px'
+    }
+  }
+
+  return (
+    <>
+      <StyledInput
+        ref={inputRef}
+        rows={1}
+        onInput={handleResizeHeight}
+      />
+    </>
+  )
+}
+
+const StyledInput = styled.textarea`
+  width: 100%;
+  padding: 10px 16px;
+  border: none;
+  border-radius: 21px;
+  max-height: 114px;
+  &:focus {
+    outline: none;
+  }
+`
