8a3cf953cb9b0103ae334935b6b0e7bf5858a7cb
leechanghwi <lih120@naver.com>
2023-09-21 14:09:02 +0900

    fix: 키값 오류수정

--- src/components/trades/uploadproduct/Account.tsx
+++ src/components/trades/uploadproduct/Account.tsx
@@ -1,30 +1,36 @@
 import { theme } from 'styles/index'
 import { css, styled } from 'styled-components'
 import { Input } from 'components/index'
-import { TRADES_ACCOUNT_TEXT } from 'constants/trades/index'
+
+const TRADES_ACCOUNT_TEXT = [
+  { id: 1, text: '금융기관을 선택해주세요.' },
+  { id: 2, text: '계좌번호 입력' },
+  { id: 3, text: '계좌주 성명' }
+]
 
 export const Account = () => {
   return (
     <>
       <Wrapper>
-        {TRADES_ACCOUNT_TEXT.map((text, index) => (
-          <Input
-            key={index}
-            $inputWidth={
-              index === 0
-                ? '350px'
-                : index === 1
-                ? '230px'
-                : index === 2
-                ? '110px'
-                : '350px'
-            }
-            $inputHeight="48px"
-            $borderColor={theme.greyScale.grey3}
-            $borderRadius="8px"
-            $paddingLeft="12px"
-            ph={text}
-          />
+        {TRADES_ACCOUNT_TEXT.map(item => (
+          <InputFocus key={item.id}>
+            <Input
+              $inputWidth={
+                item.id === 1
+                  ? '350px'
+                  : item.id === 2
+                  ? '230px'
+                  : item.id === 3
+                  ? '110px'
+                  : '350px'
+              }
+              $inputHeight="48px"
+              $borderColor={theme.greyScale.grey3}
+              $borderRadius="8px"
+              $paddingLeft="12px"
+              ph={item.text}
+            />
+          </InputFocus>
         ))}
       </Wrapper>
     </>
@@ -36,4 +42,10 @@ const Wrapper = styled.div`
   flex-wrap: wrap;
   gap: 10px;
   margin-top: 10px;
+`
+
+const InputFocus = styled.div`
+  :focus {
+    border: 2px solid ${theme.main.blue0};
+  }
 `
