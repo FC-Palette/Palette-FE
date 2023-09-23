5830c4b3d55b1b5b82713f8c90a21cd3a3917c06
hookor <gerrard1219@gmail.com>
2023-09-23 13:01:36 +0900

    fix: mediaquery height

--- src/components/MediaQuery.tsx
+++ src/components/MediaQuery.tsx
@@ -1,22 +1,31 @@
 import { useMediaQuery } from 'react-responsive'
 import { styled } from 'styled-components'
+import { useState, useEffect } from 'react'
+
 export const MediaQuery = ({ children }) => {
   const isMobile = useMediaQuery({
     query: '(max-width:431px)'
   })
   const notMobile = useMediaQuery({ query: '(min-width:431px)' })
+  const [innerHeight, setInnerHeight] = useState<number>(0)
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setInnerHeight(window.innerHeight)
+    }
+  }, [])
 
   return (
     <>
       {isMobile && children}
-      {notMobile && <Wrapper>{children}</Wrapper>}
+      {notMobile && <Wrapper $innerHeight={innerHeight}>{children}</Wrapper>}
     </>
   )
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ $innerHeight?: number }>`
   display: flex;
   justify-content: center;
   background-color: ${props => props.theme.greyScale.grey0};
+  height: ${props => props.$innerHeight}px;
   /* overflow: hidden; */
 `
