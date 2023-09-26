e72df9474ebc309e208dc78edcb7bb1345326a32
leechanghwi <lih120@naver.com>
2023-09-26 17:32:05 +0900

    fix: 오류수정

--- src/constants/trades/tradesSubHeader.ts
+++ src/constants/trades/tradesSubHeader.ts
@@ -2,3 +2,10 @@ export const TRADES_SUBHEADER_TEXT = [
   { text: '공동구매', path: '/groupPurchase' },
   { text: '중고거래', path: '/secondHand' }
 ]
+
+export const TRADES_MAIN_FILTER_ITEMS = [
+  { value: 'latest', label: '최신순' },
+  { value: 'oldest', label: '오래된순' },
+  { value: 'favoriteCount', label: '찜 많은 순' },
+  { value: 'createdAt', label: '모임시작일 순' }
+]
