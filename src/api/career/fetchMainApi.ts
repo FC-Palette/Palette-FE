7451dc00f1d3a702352130716c15119d0691e37d
ahntaeuk <dotory0829@naver.com>
2023-10-02 15:20:50 +0900

    fix: 성장해요 배포 사이트 에러 수정

--- src/api/career/fetchMainApi.ts
+++ src/api/career/fetchMainApi.ts
@@ -1,5 +1,5 @@
 import { fetchMainResponseDataProps } from '@/types'
-import { authInstance, baseInstance } from '../axios'
+import { baseInstance } from '../axios'
 
 export async function fetchMainApi(
   isClose: number,
