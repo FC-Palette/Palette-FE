ede6653ab94c962371c019890ce5f580ae60aeac
ahntaeuk <dotory0829@naver.com>
2023-10-02 15:14:51 +0900

    fix: 배포 사이트 에러 수정

--- src/utils/career/sortResponseData.ts
+++ src/utils/career/sortResponseData.ts
@@ -4,16 +4,26 @@ export interface SortedData {
   startDate: string
 }
 
-export const sortResponseData = (responseData: [], filter?: string) => {
+export enum SortFilter {
+  Latest = '최신순',
+  Oldest = '오래된순',
+  MostLiked = '찜 많은 순',
+  StartDate = '모임시작일 순'
+}
+
+export const sortResponseData = (
+  responseData: SortedData[],
+  filter: SortFilter = SortFilter.Latest
+) => {
   const compareFunction = (a: SortedData, b: SortedData) => {
     switch (filter) {
-      case '최신순':
+      case SortFilter.Latest:
         return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-      case '오래된순':
+      case SortFilter.Oldest:
         return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-      case '찜 많은 순':
+      case SortFilter.MostLiked:
         return b.likes - a.likes
-      case '모임시작일 순':
+      case SortFilter.StartDate:
         return new Date(a.startDate).getTime() - new Date(b.startDate).getTime()
       default:
         return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
