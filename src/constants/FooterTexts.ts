ec54ea11d6d010f24ca803d5ca33b0a3d8af4bc8
ahntaeuk <dotory0829@naver.com>
2023-10-05 21:59:50 +0900

    fix: 성장해요 api 병합

--- src/constants/FooterTexts.ts
+++ src/constants/FooterTexts.ts
@@ -1,7 +1,7 @@
 import { Home, Messages2, User } from 'iconsax-react'
 
 export const ICON_TEXTS = [
-  { text: '홈', icon: Home, path: '/' },
+  { text: '홈', icon: Home, path: '/home' },
   { text: '채팅', icon: Messages2, path: '/chatlist/p' },
   { text: '마이페이지', icon: User, path: '/mypage' }
 ]
